70b24f993770f7d2aa1ec1ddfbd176f9999f8289
Eric <eric.m.lombardo@gmail.com>
2021-05-12 15:47:10 -0400

    commit tree aa48d1031cf60be42bc76b8bcafda72710bfeb7b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector("#main").remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "Eric is the champion"
